c3adf369c8f28dd0546aada8f4aeedd428d485dd
Monesh Pulla <monesh6302@gmail.com>
2025-04-12 04:09:24 -0700

    Batch 13 of 20

---
+++ frontend/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/monesh/Documents/projects/team-project-202-technominds/frontend/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
